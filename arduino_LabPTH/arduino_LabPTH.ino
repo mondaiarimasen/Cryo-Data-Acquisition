02312dd9faceca6d5ccfe03b3f941e5f39846f20
Victor Zhang <victor.g.zhang@gmail.com>
2018-08-28 02:23:00 -0400

    version 1.1.0; added some comments; code unchanged since 8/21-8/24 run

--- arduino_LabPTH/arduino_LabPTH.ino
+++ arduino_LabPTH/arduino_LabPTH.ino
@@ -1,6 +1,6 @@
 // Victor Zhang, created August 20, 2018
 // Arduino program to get Temperature, Pressure, and Relative Humidity of Lab
-// version 1.0.0
+// version 1.1.0
 // Arduino
 
 
@@ -8,35 +8,35 @@
 // Library allows either I2C or SPI, so include both.
 #include "Wire.h"
 #include "SPI.h" // will be using I2C connection
- 
+
 BME280 sensor ;
- 
+
 void setup ( )  {
- 
+
    Serial. begin ( 9600 ) ;
   while  ( ! Serial )  {
     // Waiting for the serial port to open for Arduino LEONARDO
   }
   // sensor configuration
-  sensor. settings . commInterface  = I2C_MODE ; 
+  sensor. settings . commInterface  = I2C_MODE ;
   sensor. settings . I2CAddress  =  0x76 ;
-  sensor. settings . runMode  =  3 ; 
+  sensor. settings . runMode  =  3 ;
   sensor. settings . tStandby  =  0 ;
   sensor. settings . filter  =  0 ;
   sensor. settings . tempOverSample  =  1  ;
   sensor. settings . pressOverSample  =  1 ;
   sensor. settings . humidOverSample  =  1 ;
- 
+
   // loading the sensor configuration
   sensor. begin ( ) ;
-    
+
 }
- 
+
 void loop ( )  {
   Serial. print ( sensor.readTempC ( ) ) ;
   Serial.print(",");
   Serial. print ( sensor.readFloatPressure ( ) / 100 ) ; // converts from Pa to HPa
   Serial.print(",");
   Serial. println ( sensor.readFloatHumidity ( ) ) ;
-  delay ( 10*1000 ) ;
+  delay ( 10*1000 ) ; // delay has a lower limit of 2ms
 }
